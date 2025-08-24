df7fa7247e0a38bbd1ae0b070611edc49c95ab86
remanmhzn008 <mhznremon79@gmail.com>
2025-08-24 17:05:13 +0545

    jkasbd

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -6,14 +6,7 @@ export default function RegisterPage() {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[400px]">
-        <div className="flex flex-col space-y-2 text-center">
-          <h1 className="text-2xl font-semibold tracking-tight">
-            Create an account
-          </h1>
-          <p className="text-sm text-muted-foreground">
-            Enter your details to create your account
-          </p>
-        </div>
+    
         <Suspense fallback={<div>Loading...</div>}>
           <RegisterForm />
         </Suspense>
